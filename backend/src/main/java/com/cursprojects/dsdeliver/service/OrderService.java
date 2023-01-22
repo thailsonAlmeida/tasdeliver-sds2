e7f338541d4b625b33484d9e2c9e65e4d6f36b11
Thailson Almeida <103151779+thailsonAlmeida@users.noreply.github.com>
2023-01-22 15:15:28 -0300

    GET /orders

---
+++ backend/src/main/java/com/cursprojects/dsdeliver/service/OrderService.java
@@ -0,0 +1,25 @@
+package com.cursprojects.dsdeliver.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.cursprojects.dsdeliver.dto.OrderDTO;
+import com.cursprojects.dsdeliver.entities.Order;
+import com.cursprojects.dsdeliver.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository repository;
+	
+	@Transactional(readOnly = true)
+	public List<OrderDTO> findAll(){
+		List<Order> list = repository.findOrderWithProducts();
+		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
+	}
+}

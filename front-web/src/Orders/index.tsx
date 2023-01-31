08096a237a13223b2182d290b5121a1e27e58acf
Thailson Almeida <103151779+thailsonAlmeida@users.noreply.github.com>
2023-01-30 22:20:22 -0300

    Add Selected

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -5,9 +5,13 @@ import './styles.css';
 import { OrderLocationData, Product } from './types';
 import { fetchProducts } from '../api';
 import OrderLocation from './OrderLocation';
+import OrderSummary from './OrderSummary';
+import Footer from '../Footer';
+import { checkIsSelected } from './helpers';
 
 function Orders () {
-    const [products, setProducts] = useState<Product[]>([]);    
+    const [products, setProducts] = useState<Product[]>([]);  
+    const [selectedProducts, setSelectedProducts] = useState<Product[]>([]); 
     const [orderLocation, setOrderLocation] = useState<OrderLocationData>();
 
     useEffect(() => {
@@ -16,12 +20,34 @@ function Orders () {
             .catch(error => console.log(error));
     }, []);
 
+    const handleSelectProduct = (product: Product) => {
+        const isAlreadySelected = checkIsSelected(selectedProducts, product);
+      
+        if (isAlreadySelected) {
+          const selected = selectedProducts.filter(item => item.id !== product.id);
+          setSelectedProducts(selected);
+        } else {
+          setSelectedProducts(previous => [...previous, product]);
+        }
+      }
+
     return(
-        <div className='orders-container'>
+        <>
+            <div className='orders-container'>
             <StepsHeader />
-            <ProductsList products={products} />
-            <OrderLocation onChangeLocation={location => setOrderLocation(location)} />
-        </div>
+            <ProductsList 
+                products={products}
+                onSelectProduct={handleSelectProduct}
+                selectedProducts={selectedProducts}
+            />
+            <OrderLocation 
+                onChangeLocation={location => setOrderLocation(location)} 
+            />
+            <OrderSummary />            
+            </div>
+            <Footer />
+        </>
+        
     );
 }
 export default Orders;
